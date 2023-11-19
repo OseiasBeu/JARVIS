ef39cd6ba88741beaebe4d9b2796300f4c906349
beu.oseias <beu.oseias@ifood.com.br>
2023-11-19 10:55:50 -0300

    Remover arquivos antigos, e adicinar linha de identificação do Ethan

--- ethan.py
+++ ethan.py
@@ -1,5 +1,6 @@
 from modules import listen, backbone,speak
 if __name__ == "__main__":
+    print('Eu sou: E.T.H.A.N > Entidade Tecnológica de Assistência Natural')
     while True:
         comando = listen.listen_function()
         response = backbone.execute_command(comando)
@@ -9,4 +10,3 @@ if __name__ == "__main__":
             speak.speak_function('Sequência de desligamento iniciada!')
             speak.speak_function('Até logo, Senhor!')
             break
-        
